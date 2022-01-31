99995f868b48348a3d8261476ef7da7e02d8a20e
Borislav Ivanov <bsivanov@gmail.com>
2022-01-31 23:35:04 +0200

    Add support for Visual Studio 2022

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyTitle("Open XML Package Editor for Visual Studio")]
 [assembly: AssemblyDescription("Open XML Package Editor for Visual Studio")]
 [assembly: AssemblyCompany("Borislav Ivanov")]
 [assembly: AssemblyProduct("Open XML Package Editor for Visual Studio")]
-[assembly: AssemblyCopyright("Copyright (c) 2017-2019, Borislav Ivanov")]
+[assembly: AssemblyCopyright("Copyright (c) 2017-2022, Borislav Ivanov")]
